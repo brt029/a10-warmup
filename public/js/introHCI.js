eecdc62b9b8c42c59ac5fe97d506e65baf5980ae
Bradley Tran <brt029@ucsd.edu>
2020-03-03 09:46:53 -0800

    Google analytics

---
+++ public/js/introHCI.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	initializePage();
+});
+
+function initializePage() {
+	$('.linkClick').click(function(e)
+	{
+		e.preventDefault();
+		ga("send", "event", "link", "click");
+	});
+	$('.linkCtr').click(function(e){
+		e.preventDefault();
+		ga("send", "event", "link", "click");
+	});
+}
+
